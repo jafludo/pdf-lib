eaf5c983b19bd61c99a0311004661dc28cef4eb2
jafludo <60601699+jafludo@users.noreply.github.com>
2021-08-19 15:37:27 +0200

    front

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');  
 const dotenv = require('dotenv');
 const app = express();
+const http = require('http');
+
+//Server Web HTTP
+var server = http.createServer(app).listen("7000", function(){
+ 
+});
 
 dotenv.config();
+
 app.use(cors({credentials: true, origin: 'http://127.0.0.1:7000'}));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+
 app.use(express.static('../public'));
